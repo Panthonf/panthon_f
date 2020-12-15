f471fd4ed34b9f7ba48fd7896e4f58f893043d01
Panthonf <panthon_kansap@cmu.ac.th>
2020-12-15 16:22:56 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x,y,i;
+        cout << "Enter an integer: ";
+        cin>>x;
+         while(x!=0){
+             cout << "Enter an integer: ";
+             cin>>x;
+             if(x==0){
+                 break;}
+             if(x%2==0){
+                 y++;
+    }
+    else{
+       i++;
+        
+        }
+
+    cout << "#Even numbers = "<<y<<"\n";
+    cout << "#Odd numbers = "<<i;
     return 0;
 }
